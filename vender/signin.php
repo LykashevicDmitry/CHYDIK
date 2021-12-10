3ae5b101c3a54c7852599229db6f57e814afa21f
LykashevicDmitry <nazritplay@gmail.com>
2021-12-11 03:03:06 +0500

    master

--- vender/signin.php
+++ vender/signin.php
@@ -2,11 +2,32 @@
     session_start();
     require_once 'connect.php';
     $connect = mysqli_connect('localhost','root','','test');
-
-
     $login= $_POST['login'];
-    $password= md5($_POST['password ']);
+    $password= $_POST['password '];
+    $erroe_fields=[];
 
+    if ($login === ''){
+        $erroe_fields[] ='login';
+    }
+
+    if ($password === ''){
+        $erroe_fields[] ='password';
+    }
+
+    if (!empty($erroe_fields)) {
+        $response = [
+            "status"=>false,
+            "type"=>1,
+            "message"=>"Проверте правильность полей",
+            "fields"=>$erroe_fields
+            ];
+
+        echo json_encode($response);
+
+        die();
+
+    }
+    $password= md5($_POST['password ']);
     $check_user=mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password`= '$password'");
     echo  mysqli_num_rows($check_user);
     if (mysqli_num_rows($check_user)>0){
@@ -17,9 +38,18 @@
             "avatar"=>$user['avatar'],
             "email"=>$user['eamil']
     ];
-        header('Location: ../profile.php');
+        $response=[
+            "status"=>true
+        ];
+        //header('Location: ../profile.php');
+        echo json_encode($response);
     }else{
-        $_SESSION['massge']= 'ты тупой';
-        header('Location:../authorization.php');
+        //$_SESSION['massge']= 'ты тупой';
+        //header('Location:../authorization.php');
+        $response=[
+            "status"=>false,
+            "message"=>'неверный логин или пороль'
+        ];
+        echo json_encode($response);
     }
-
+?>
--- vender/signup.php
+++ vender/signup.php
@@ -9,21 +9,84 @@
     $password= $_POST['password '];
     $password_confirm= $_POST['password_confirm'];
 
+    $check_login=mysqli_query($connect,"SELECT * FROM `user` WHERE `login`= `$login`");
+    if (mysqli_num_rows($check_login)>0){
+        $response = [
+            "status"=>false,
+            "type"=>1,
+            "message"=>"логин занят",
+            "fields"=>['login']
+        ];
+        echo json_encode($response);
+        die();
+    }
+
+$erroe_fields=[];
+
+if ($login === ''){
+    $erroe_fields[] ='login';
+}
+
+if ($password === ''){
+    $erroe_fields[] ='password';
+}
+if ($full_name === ''){
+    $erroe_fields[] ='full_name';
+}
+if ($email === '' || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $erroe_fields[] ='email';
+}
+if (!$_FILES['avatar']){
+    $erroe_fields[] ='avatar';
+}
+if ($password_confirm === ''){
+    $erroe_fields[] ='$password_confirm';
+}
+
+if (!empty($erroe_fields)) {
+    $response = [
+        "status"=>false,
+        "type"=>1,
+        "message"=>"Проверте правильность полей",
+        "fields"=>$erroe_fields
+    ];
+
+    echo json_encode($response);
+
+    die();
+
+}
+
     if ($password === $password_confirm){
 
         //$_FILES['avatar']['name'];
         $path='uploads/' . time() .$_FILES['avatar']['name'];
         if (!move_uploaded_file($_FILES['avatar']['tmp_name'],'../' . $path)){
-            $_SESSION['massge']= 'картинка калл меняй';
-            header('Location:../register.php  ');
+            $response = [
+                "status"=>false,
+                "type"=>2,
+                "message"=>"КАртинка калл меняй",
+            ];
+
+            echo json_encode($response);
         }
         $password=md5($password);
         mysqli_query($connect, "INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES ('', $full_name, $login, $email, $password, $path)");
-        $_SESSION['massge']= 'регистрация прошла успешно';
-        header('Location:../authorization.php  ');
+
+        $response = [
+            "status"=>true,
+            "message"=>"регистрация прошла успешно",
+        ];
+
+        echo json_encode($response);
+
     }else{
-        $_SESSION['massge']= 'пароли не совпадают колун';
-        header('Location:../register.php  ');
+        $response = [
+            "status"=>false,
+            "message"=>"пороли не совпадают",
+        ];
+
+        echo json_encode($response);
     }
 
 
