43de50e10fb4f417e1cdc3952251dcc91af1fef3
LykashevicDmitry <74460099+LykashevicDmitry@users.noreply.github.com>
2021-12-11 11:32:08 +0500

    master

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
 /*Авторизизация*/
 
-$('.login-btn').click(function (e) {
+$('.login-btn').click(function(e) {
     e.preventDefault();
     $(`input`).removeClass('error');
 
-    let login=$('input[name="login"]').val(),
-         password=$('input[name="passwoed"]').val();
+    let login = $('input[name="login"]').val(),
+        password = $('input[name="passwoed"]').val();
 
     $.ajax({
-       url: 'vendor/signin.php',
+        url: 'vendor/signin.php',
         type: 'POST',
         dataType: 'json',
-        data:{
-           login: login,
+        data: {
+            login: login,
             password: password
         },
-        success (data){
-           if (data.status){
-               document.location.href= '/profile.php';
-           }
-           else {
-               if (data.type ===1){
-                    data.fields.forEach(function (field){
+        success(data) {
+            if (data.status) {
+                document.location.href = '/profile.php';
+            } else {
+                if (data.type === 1) {
+                    data.fields.forEach(function(field) {
                         $(`input[name="${field}"]`).addClass('error')
                     });
-               }
+                }
 
-               $('.msg').removeClass('none').text(data.message);
-           }
+                $('.msg').removeClass('none').text(data.message);
+            }
 
         }
     });
@@ -37,32 +36,32 @@ $('.login-btn').click(function (e) {
 Загрузка аватарки с поля
  */
 
-let avatar = true;
+let avatar = false;
 
-$('input[name="avatar"]').change(function (e) {
-    avatar= e.target.files[0];
+$('input[name="avatar"]').change(function(e) {
+    avatar = e.target.files[0];
 })
 
 
 /*Регистрация */
 
-$('.register-btn').click(function (e) {
+$('.register-btn').click(function(e) {
     e.preventDefault();
     $(`input`).removeClass('error');
 
-    let login=$('input[name="login"]').val(),
-        password=$('input[name="passwoed"]').val(),
-        full_name=$('input[name="fullname"]').val(),
-        email=$('input[name="email"]').val(),
-        password_confirm=$('input[name="password_confirm"]').val();
+    let login = $('input[name="login"]').val(),
+        password = $('input[name="passwoed"]').val(),
+        full_name = $('input[name="fullname"]').val(),
+        email = $('input[name="email"]').val(),
+        password_confirm = $('input[name="password_confirm"]').val();
 
     let fromData = new FormData();
-    formData.append('login',login);
-    formData.append('passworf',password);
-    formData.append('full_name',full_name);
-    formData.append('email',email);
-    formData.append('password_confirm',password_confirm);
-    formData.append('avatar',avatar);
+    formData.append('login', login);
+    formData.append('passworf', password);
+    formData.append('full_name', full_name);
+    formData.append('email', email);
+    formData.append('password_confirm', password_confirm);
+    formData.append('avatar', avatar);
 
 
 
@@ -74,13 +73,12 @@ $('.register-btn').click(function (e) {
         contentType: false,
         cache: false,
         data: fromData,
-        success (data){
-            if (data.status){
-                document.location.href= '/authorization.php';
-            }
-            else {
-                if (data.type ===1){
-                    data.fields.forEach(function (field){
+        success(data) {
+            if (data.status) {
+                document.location.href = '/authorization.php';
+            } else {
+                if (data.type === 1) {
+                    data.fields.forEach(function(field) {
                         $(`input[name="${field}"]`).addClass('error')
                     });
                 }
